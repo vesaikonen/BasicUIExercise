d82015186d490389e92b172c7375c35a216a3033
vesaikonen <H8614@student.jamk.fi>
2016-09-20 13:53:01 +0300

    BasicUIExercise
    Demo 02 BasicUI

---
+++ app/src/main/java/fi/jamk/basicuiexercise/MainActivity.java
@@ -0,0 +1,29 @@
+package fi.jamk.basicuiexercise;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void selectButtonClicked(View view) {
+        // find radiogroup
+        RadioGroup rg = (RadioGroup) findViewById(R.id.radioGroup);
+        int id = rg.getCheckedRadioButtonId();
+        // find button
+        RadioButton rb = (RadioButton) findViewById(id);
+        // get radiobutton text
+        String text = (String) rb.getText();
+        // toast message to screen
+        Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT).show();
+    }
+}
